e2ed4c4d11d875608ae145bb2777e0c194e27c17
usmansafderktk <usman.safder2021@namal.edu.pk>
2023-10-22 22:37:12 +0500

    Adding feature 2

--- README.md
+++ README.md
@@ -3,23 +3,41 @@
 
 <p>I got nothing to say. Talk is overrated.</p>
 
-<h>Library Management System</h1>
+<h1>Library Management System</h2>
 
-This is a simple Python application for managing a library. It allows you to add books to the library and display the list of available books.
+This Library Management System is a Python program that allows users to register, log in, and manage their library-related tasks. It is designed for simplicity and ease of use.
 
 <h2>Features</h2>
 
-- Add books to the library.
-- Display the list of available books.
+User Registration: Users can create new accounts by providing a username and password. Duplicate usernames are not allowed.
 
-## How to Use
+User Login: Registered users can log in with their username and password.
 
-1. Clone the repository to your local machine.
-2. Ensure you have Python installed.
-3. Open your terminal or command prompt and navigate to the project directory.
-4. Run the application using the following command:
+<h2>How to Use</h2>
+Clone the Repository: Clone this repository to your local machine using Git:
 
-I created two branches. My colleage forked the repository and then we merged the both brances.
+bash
+Copy code
+git clone https://github.com/your-username/library-management-system.git
+Run the Program: Execute the main program file, e.g., main.py, in your Python environment.
 
+User Registration:
 
+When you run the program, you will be prompted to enter a new username and password.
+If the username is unique, your registration will be successful.
+User Login:
 
+After registration, you can log in using your username and password.
+Library Management:
+
+This system is designed for educational purposes, and you can expand it to manage library tasks like borrowing books, returning books, and maintaining a book catalog.
+<h2>Contribution</h2>
+This project is a collaborative effort by two university students Usman and Zami. If you'd like to contribute, follow these steps:
+
+Fork the repository to your GitHub account.
+
+Clone your forked repository to your local machine.
+
+Make your changes or enhancements to the code.
+
+Create a new branch and make a pull request to the original repository for review.
--- code.py
+++ code.py
@@ -1,72 +1,23 @@
-class Book:
-    def __init__(self, title, author, available_copies):
-        self.title = title
-        self.author = author
-        self.available_copies = available_copies
-
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, book):
-        self.books.append(book)
-
-    def display_books(self):
-        for book in self.books:
-            print(f"Title: {book.title}, Author: {book.author}, Available Copies: {book.available_copies}")
-
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-def main():
-    users = [User("user1", "pass1"), User("user2", "pass2")]
-    logged_in_user = None
-
+def register(users):
     while True:
-        if logged_in_user is None:
-            print("\nLibrary Management System")
-            print("1. Register")
-            print("2. Login")
-            print("3. Exit")
-            choice = input("Enter your choice: ")
+        username = input("Enter a new username: ")
+        password = input("Enter a password: ")
 
-            if choice == '1':
-                register(users)
-            elif choice == '2':
-                logged_in_user = login(users)
-            elif choice == '3':
-                print("Goodbye!")
-                break
+        if any(user.username == username for user in users):
+            print("Username already exists. Please choose another username.")
         else:
-            print(f"\nWelcome, {logged_in_user.username}!")
-            print("1. Add Book")
-            print("2. Display Books")
-            print("3. Logout")
-            choice = input("Enter your choice: ")
+            users.append(User(username, password))
+            print("Registration successful!")
+            break
 
-            if choice == '1':
-                title = input("Enter book title: ")
-                author = input("Enter author: ")
-                available_copies = int(input("Enter available copies: "))
-                book = Book(title, author, available_copies)
-                library.add_book(book)
-                print("Book added successfully!")
+def login(users):
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
 
-            elif choice == '2':
-                library.display_books()
+        for user in users:
+            if user.username == username and user.password == password:
+                print("Login successful!")
+                return user
 
-            elif choice == '3':
-                logged_in_user = None
-
-if __name__ == "__main__":
-    library = Library()
-    main()
-    
-    
-    
-    
-    
-
+        print("Invalid username or password. Please try again.")
